d2799800dd6106fcba2dfc50fcfbbe67545c6e3d
Bouke van der Bijl <i@bou.ke>
2024-06-06 22:29:52 +0200

    initial

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AprilTags",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AprilTags",
+            targets: ["AprilTags"]),
+    ],
+    targets: [
+        .target(
+            name: "CAprilTags"),
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AprilTags"),
+        .testTarget(
+            name: "AprilTagsTests",
+            dependencies: ["AprilTags"]),
+    ]
+)
